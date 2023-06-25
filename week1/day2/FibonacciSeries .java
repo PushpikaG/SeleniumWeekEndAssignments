4fec62d5fa8b600bb96e129003e53707a6aa9b41
PushpikaG <137673002+PushpikaG@users.noreply.github.com>
2023-06-25 11:18:02 +0530

    Pushpika FibonacciSeries .java

--- week1/day2/FibonacciSeries .java
+++ week1/day2/FibonacciSeries .java
@@ -1,36 +1,23 @@
-package week1.day1.assignments;
+package week1.day1;
 
-public class FibonacciSeries {
-
-	/*
-	 * Goal: To find Fibonacci Series for a given range
-	 * 
-	 * input(range): 8 output: 0, 1, 1, 2, 3, 5, 8, 13
-	 * 
-	 * Shortcuts: 1) Print : type: syso, followed by: ctrl + space + enter 2) To
-	 * create a 'for' loop: type 'for', followed by ctrl + space + down arrow +
-	 * enter
-	 * 
-	 * What are my learnings from this code? 1) 2) 3)
-	 * 
-	 */
-
+public class Fibonacciassignment {
 	public static void main(String[] args) {
+		int range=8;
+		int firstNum=0;
+		int secondNum=1;
+		System.out.println("Firstnumber "+firstNum);
+		for(int i=1;i<=range;i++)
+		{
+			int sum=firstNum+secondNum;
+            firstNum=secondNum;
+            secondNum=sum;
+            System.out.println("Fibonacci series is"+sum);
+      
+            
 
-		// initialize 3 int variables (Tip: range = 8, firstNum = 0, secNum = 1, sum in the series)
-
-		// Print first number
-		
-		// Iterate from 1 to the range
-		
-		// add first and second number assign to sum
-
-		// Assign second number to the first number
-
-		// Assign sum to the second number
-		
-		// print sum
-
+			
+		}
+	
 	}
 
 }
